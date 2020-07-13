34307b6862f401d94fe44ba03be88d8927533dff
Paul Tanasa <paul@programmer.net>
2020-07-13 11:48:23 +0300

    [ADD] Express middleware

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const queryFilter = require('./src/query-filter.js');
 const uriFilter = require('./src/uri-filter.js');
 
+function fuckClid(filterArray = ['fbclid', 'gclid']) {
+  const filter = {};
+
+  for (const key of filterArray) {
+    filter[key] = true;
+  }
+
+  return (request, response, next) => {
+    const uri = uriFilter(request.path, request.query, filter);
+    
+    if (uri === null) {
+      return next();
+    }
+
+    response.redirect(302, uri);
+  };
+}
+
+module.exports = fuckClid;
+
 module.exports.queryFilter = queryFilter;
 module.exports.uriFilter = uriFilter;
