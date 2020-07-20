05eae964e8fffeaaa6ff184a8688e43355858537
Paul Tanasa <paul@programmer.net>
2020-07-20 12:42:06 +0300

    [FIX] Types

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,4 @@
-import { Request, RequestHandler } from 'express';
-
+import { RequestHandler } from 'express';
 declare function noClid(filter?: string[]): RequestHandler;
-declare function queryFilter(query: Request['query'], filter: object): null | object;
-declare function uriFilter(path: Request['path'], query: Request['query'], filter: object): null | string;
 
 export = noClid;
-export { queryFilter, uriFilter };
